337b24e345ee2452187d763b81d92f4972108209
liuliang <454493675@qq.com>
2019-02-22 09:04:17 +0800

    Initial Commit

---
+++ amei/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  amei
+//
+//  Created by apple on 2019/2/22.
+//  Copyright © 2019 corzen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ amei/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  amei
+//
+//  Created by apple on 2019/2/22.
+//  Copyright © 2019 corzen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
